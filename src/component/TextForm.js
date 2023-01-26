05c75a154ebc9dc80d186d8d25eb68c34b4d87d6
Udit Mishra <294uditmishra@gmail.com>
2023-01-26 20:24:27 +0530

    Counting words and counting characters is added

--- src/component/TextForm.js
+++ src/component/TextForm.js
@@ -17,13 +17,23 @@ export default function TextForm(props) {
     const [text,setText]=useState('Enter text here');
  
     return (
-        <div>
+        <>
+        <div className='container'>
           <h2>{props.heading}</h2>
       <div className="form-group">
       <textarea className="form-control" id="mybox" rows="8"  value={text} onChange={handleOnChange}/>
     </div>
    <button className="btn btn-primary" onClick={handleUpClick}>Convert to Upper Case</button>
      </div>
+
+     <div className="container  my-3">
+
+
+    <h1>Your text summary</h1>
+    <p>Text contains {text.split(" ").length} words and {text.length}  characters</p>
+     </div>
+     </>
+
   
  )
 }
